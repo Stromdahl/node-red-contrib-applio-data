c8abf1ed6b1575c72cdb1681bcecc03f079f2ef4
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2022-01-14 10:37:01 +0100

    Added measurment-interval node

--- applio-data/applio-measurment-interval.js
+++ applio-data/applio-measurment-interval.js
@@ -4,24 +4,28 @@ module.exports = function(RED) {
 
         var node = this;
         var devID = n.devid
+        var measurment = n.measurment
+        var start = n.start;
+        var end = n.end;
         node.on('input', function(msg, send, done) {
             var axios = require('axios');
             this.config = RED.nodes.getNode(n.config);
             token = this.config.token
             msg.payload = "";
             
-            var data = JSON.stringify({
-                "start": "-7d",
-                "end": "0d"
-              });
+            var data = {
+                start: start,
+                end: end, 
+            };
 
             var config = {
-                method: 'get',
-                url: `https://data.applio.tech/data/device/${devID}/measurement/list`,
+                method: 'post',
+                url: `https://data.applio.tech/data/device/${devID}/${measurment}/list`,
                 headers: { 
                     'accept': 'application/json', 
                     'Grpc-Metadata-Authorization': token
-                }
+                },
+                data: data
             };
 
             axios(config)
@@ -35,5 +39,5 @@ module.exports = function(RED) {
         });
 
     }
-    RED.nodes.registerType("applio-measurment-list", ApplioDeviceNode);
+    RED.nodes.registerType("applio-measurment-interval", ApplioDeviceNode);
 }
